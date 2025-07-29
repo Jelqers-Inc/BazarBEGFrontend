f7ca8523fd72e1ed797e9dbc2beb2ee9033b8a7a
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-07-29 10:26:13 -0600

    Iniciando archivos

---
+++ src/main/java/org/esfr/BazarBEG/BazarBegApplication.java
@@ -0,0 +1,13 @@
+package org.esfr.BazarBEG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarBegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarBegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfr/BazarBEG/BazarBegApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfr.BazarBEG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarBegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
