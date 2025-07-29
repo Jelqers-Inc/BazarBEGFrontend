700cc69dc90454b1df09b81c5f53b6e4b844b5cf
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-07-29 10:49:40 -0600

    Modelos creados

--- src/main/java/org/esfr/BazarBEG/modelos/usuario.java
+++ src/main/java/org/esfr/BazarBEG/modelos/usuario.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Usuario {
+public class usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
