08f52f76033d6422ac2b0ba4f9403fb3176092ad
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-08-14 11:11:39 -0600

    Modelo catalogo creado

--- src/main/java/org/esfr/BazarBEG/modelos/Catalogo.java
+++ src/main/java/org/esfr/BazarBEG/modelos/Catalogo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+//Modelo categoria creado, para asociar commit
 
 @Entity
 @Table(name = "catalogos")
