90ad8b608f902f08dc7fa4d3428af5df6f45009e
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-08-14 11:47:50 -0600

    Se creo IUsuarioService

---
+++ src/main/java/org/esfr/BazarBEG/servicios/interfaces/IUsuarioService.java
@@ -0,0 +1,20 @@
+package org.esfr.BazarBEG.servicios.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.esfr.BazarBEG.modelos.Usuario;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
+public interface IUsuarioService {
+    Page<Usuario> buscarTodosPaginados(Pageable pageable);
+
+    List<Usuario> obtenerTodos();
+
+    Optional<Usuario> buscarPorId(Integer id);
+
+    Usuario crearOEditar(Usuario usuario);
+
+    void eliminarPorId(Integer id);
+}
