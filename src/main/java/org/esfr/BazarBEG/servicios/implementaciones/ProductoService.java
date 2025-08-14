54b6b303bd9179244668e6f8f955bbd7935a4ed5
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-08-14 15:26:53 -0600

    Se créo el servicio para Producto

---
+++ src/main/java/org/esfr/BazarBEG/servicios/implementaciones/ProductoService.java
@@ -0,0 +1,47 @@
+package org.esfr.BazarBEG.servicios.implementaciones;
+
+import org.esfr.BazarBEG.modelos.Producto;
+import org.esfr.BazarBEG.repositorios.IProductoRepository;
+import org.esfr.BazarBEG.servicios.interfaces.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+
+@Service
+public class ProductoService implements IProductoService {
+
+    @Autowired
+    private IProductoRepository productoRepository;
+
+
+    public Page<Producto> buscarTodosPaginados(String nombre, String categoriaNombre, Pageable pageable) {
+        return productoRepository.findByNombreContainingIgnoreCaseOrCategoria_NombreContainingIgnoreCase(
+                nombre, categoriaNombre, pageable
+        );
+    }
+
+    @Override
+    public List<Producto> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Optional<Producto> buscarPorId(Integer id) {
+        return productoRepository.findById(id);
+    }
+
+    @Override
+    public Producto crearOEditar(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        productoRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/org/esfr/BazarBEG/servicios/interfaces/IProductoService.java
@@ -0,0 +1,20 @@
+package org.esfr.BazarBEG.servicios.interfaces;
+
+import org.esfr.BazarBEG.modelos.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductoService {
+    Page<Producto> buscarTodosPaginados(String nombre, String categoriaNombre,Pageable pageable);
+
+    List<Producto> obtenerTodos();
+
+    Optional<Producto> buscarPorId(Integer id);
+
+    Producto crearOEditar(Producto producto);
+
+    void eliminarPorId(Integer id);
+}
