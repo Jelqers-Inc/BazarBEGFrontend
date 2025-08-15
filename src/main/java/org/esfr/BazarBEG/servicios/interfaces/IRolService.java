2944d444a3a2a5c2472334c0d1426b23fbe911f8
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-08-15 09:53:15 -0600

    Se creo el servicio para Rol

---
+++ src/main/java/org/esfr/BazarBEG/servicios/interfaces/IRolService.java
@@ -0,0 +1,20 @@
+package org.esfr.BazarBEG.servicios.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.esfr.BazarBEG.modelos.Rol;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IRolService{
+Page<Rol> buscarTodosPaginados(Pageable pageable);
+
+    List<Rol> obtenerTodos();
+
+    Optional<Rol> buscarPorId(Integer id);
+
+    Rol crearOEditar(Rol rol);
+
+    void eliminarPorId(Integer id);
+}
