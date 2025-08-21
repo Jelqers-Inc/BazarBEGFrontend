23e2fe977504e0b00c1845ef1a026bd70e1ef0e5
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-08-21 11:24:59 -0600

    Se hizo que las imagenes de categorías tambien se actualicen o se creen en el index principal

--- src/main/java/org/esfr/BazarBEG/controladores/HomeController.java
+++ src/main/java/org/esfr/BazarBEG/controladores/HomeController.java
@@ -1,15 +1,31 @@
 package org.esfr.BazarBEG.controladores;
 
+import org.esfr.BazarBEG.modelos.Categoria;
+import org.esfr.BazarBEG.servicios.interfaces.ICategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+import java.util.List;
 
 @Controller
+@RequestMapping("/") //este controlador maneja la raíz
 public class HomeController {
+
+    @Autowired
+    private ICategoriaService categoriaService;
+
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        // Obtener todas las categorías de la base de datos
+        List<Categoria> categorias = categoriaService.obtenerTodos();
+
+        // Agregar la lista de categorías al modelo
+        model.addAttribute("categorias", categorias);
+
+        // Devolver la vista (el archivo home/index.html)
         return "home/index";
     }
 }
